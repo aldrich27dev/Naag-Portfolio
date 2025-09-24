41a64a3099982764688ff05a9bdb6b7b0dda7354
PhoenixBlaze1209 <genesissamson04@gmail.com>
2025-09-24 21:26:56 +0800

    updated

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -813,8 +813,9 @@
 
           <h3 class="h3 form-title">Contact Form</h3>
 
-          <form action="#" class="form" data-form>
+          <form action="{{ url('/contact') }}" method="POST"class="form" data-form>
 
+            @csrf
             <div class="input-wrapper">
               <input type="text" name="fullname" class="form-input" placeholder="Full name" required data-form-input>
 
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortfolioControllerr;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ContactMail;
 
 Route::get('/', [PortfolioControllerr::class, 'index']);
+
+Route::post('/contact', function (Request $request) {
+    // validate input
+    $request->validate([
+        'fullname' => 'required|string|max:255',
+        'email' => 'required|email',
+        'message' => 'required|string'
+    ]);
+
+    // send email
+    Mail::to('samsongenesis5@gmail.com')->send(new ContactMail(
+        $request->fullname,
+        $request->email,
+        $request->message
+    ));
+
+    return back()->with('success', 'Your message has been sent!');
+});
